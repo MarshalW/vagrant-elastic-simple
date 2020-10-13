42a8d5aa9fa5965817f840f6ff147f90390cddd2
marshal <marshal.wu@qq.com>
2020-10-13 20:50:08 +0800

    change array usage

--- mock-logger/lib/config.js
+++ mock-logger/lib/config.js
@@ -15,3 +15,21 @@ export const AVERAGE_EVENTS = 5
 export const DELAY_MIN = 10
 // 事件最大间隔(ms)
 export const DELAY_MAX = 1000 * 60
+
+
+
+// export const DYAS_AGO = 365
+// // 生成的用户总数
+// export const USER_COUNT = 10000 * 100 * 5
+// // 生成的游戏总数
+// export const GAME_COUNT = 20
+
+// // 后续日常模拟数据的参数
+// // 一次循环中选择的用户数
+// export const CHOOSE_USERS_COUNT = 100
+// // 每个用户平均模拟事件次数
+// export const AVERAGE_EVENTS = 5
+// // 事件最小间隔(ms)
+// export const DELAY_MIN = 10
+// // 事件最大间隔(ms)
+// export const DELAY_MAX = 1000 * 20
--- mock-logger/lib/mock-data-generator.js
+++ mock-logger/lib/mock-data-generator.js
@@ -8,18 +8,40 @@ async function generate(timeRange, userCount, gameCount) {
     let users = userGenerate(timeRange, userCount)
     let games = gameGegerate(gameCount)
 
-    let eventList = []
+    // let eventList = []
+
+    // map ['2020-01-01',[events], ..]
+    let eventsDays = new Map()
+    // let currentTime = new Date().getTime()
+
+    for (let time = timeRange.start; time <= timeRange.end; time = time + 1000 * 60 * 60 * 24) {
+        eventsDays.set(time, [])
+    }
+
 
     for (const user of users) {
         let game = chooseOneGame(games)
         let events = eventsGenerate(user, timeRange, game)
-        eventList.push(...events)
+        // eventList.push(...events)
+        events.forEach(event => {
+            let time = new Date(event['@timestamp']).setHours(0, 0, 0, 0)
+            let list = eventsDays.get(time)
+            if (list == null) throw `can not find event list for ${time}`
+            list.push(event)
+        })
     }
 
-    eventList.sort((a, b) => a['@timestamp'] - b['@timestamp'])
+    // eventList.sort((a, b) => a['@timestamp'] - b['@timestamp'])
 
-    for (let event of eventList) {
-        console.log(JSON.stringify(event))
+    // for (let event of eventList) {
+    //     console.log(JSON.stringify(event))
+    // }
+
+    for (let time = timeRange.start; time <= timeRange.end; time = time + 1000 * 60 * 60 * 24) {
+        let events = eventsDays.get(time)
+        for (let event of events) {
+            console.log(JSON.stringify(event))
+        }
     }
 
     await forever(users, games)
