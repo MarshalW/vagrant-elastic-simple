f7546fd38d2df292377f6ee0ae4ea30b7d786d25
marshal <marshal.wu@qq.com>
2020-10-14 16:03:56 +0800

    use sqlite cache events

--- mock-logger/lib/mock-data-generator.js
+++ mock-logger/lib/mock-data-generator.js
@@ -3,33 +3,49 @@ import userGenerate from './user-generator'
 import gameGegerate from './game-generator'
 import eventsGenerate from './events-generator'
 import forever from './events-forever'
+import sqlite3 from 'sqlite3'
+import { open } from 'sqlite'
 
 async function generate(timeRange, userCount, gameCount) {
     let users = userGenerate(timeRange, userCount)
     let games = gameGegerate(gameCount)
 
-    // let eventList = []
-
-    // map ['2020-01-01',[events], ..]
-    let eventsDays = new Map()
-    // let currentTime = new Date().getTime()
-
-    for (let time = timeRange.start; time <= timeRange.end; time = time + 1000 * 60 * 60 * 24) {
-        eventsDays.set(time, [])
-    }
-
+    const db = await open({ filename: ':memory:', driver: sqlite3.Database })
+    await db.exec("CREATE TABLE IF NOT EXISTS history (id INTEGER PRIMARY KEY AUTOINCREMENT,time INTEGER, content TEXT)")
 
     for (const user of users) {
         let game = chooseOneGame(games)
         let events = eventsGenerate(user, timeRange, game)
-        // eventList.push(...events)
-        events.forEach(event => {
-            let time = new Date(event['@timestamp']).setHours(0, 0, 0, 0)
-            let list = eventsDays.get(time)
-            if (list == null) throw `can not find event list for ${time}`
-            list.push(event)
-        })
+
+        for (const event of events) {
+            await db.run("insert into history(time, content) values (?, ?)", event['@timestamp'], JSON.stringify(event))
+        }
     }
+
+    await db.run('CREATE INDEX idx_cache_time ON history (time)')
+
+    // let eventList = []
+
+    // map ['2020-01-01',[events], ..]
+    // let eventsDays = new Map()
+    // let currentTime = new Date().getTime()
+
+    // for (let time = timeRange.start; time <= timeRange.end; time = time + 1000 * 60 * 60 * 24) {
+    //     eventsDays.set(time, [])
+    // }
+
+
+    // for (const user of users) {
+    //     let game = chooseOneGame(games)
+    //     let events = eventsGenerate(user, timeRange, game)
+    //     // eventList.push(...events)
+    //     events.forEach(event => {
+    //         let time = new Date(event['@timestamp']).setHours(0, 0, 0, 0)
+    //         let list = eventsDays.get(time)
+    //         if (list == null) throw `can not find event list for ${time}`
+    //         list.push(event)
+    //     })
+    // }
 
     // eventList.sort((a, b) => a['@timestamp'] - b['@timestamp'])
 
@@ -37,14 +53,14 @@ async function generate(timeRange, userCount, gameCount) {
     //     console.log(JSON.stringify(event))
     // }
 
-    for (let time = timeRange.start; time <= timeRange.end; time = time + 1000 * 60 * 60 * 24) {
-        let events = eventsDays.get(time)
-        for (let event of events) {
-            console.log(JSON.stringify(event))
-        }
-    }
+    // for (let time = timeRange.start; time <= timeRange.end; time = time + 1000 * 60 * 60 * 24) {
+    //     let events = eventsDays.get(time)
+    //     for (let event of events) {
+    //         console.log(JSON.stringify(event))
+    //     }
+    // }
 
-    await forever(users, games)
+    // await forever(users, games)
 }
 
 function chooseOneGame(games) {
